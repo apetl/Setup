c343723daa24be6065f13f3d64227620afddf17a
apetl <git@apetl.dev>
2022-05-30 17:07:21 -0400

    Initial commit

---
+++ bsetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update -y &&
+sudo apt upgrade -y &&
+
+#zsh
+sudo apt install zsh -y &&
+sudo apt install git -y &&
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
