380d037f294db3a204cf704f82e5a15907c144c4
Ayan Petlikar <petlikarayan@gmail.com>
2022-11-30 18:30:41 -0500

    fixes

--- bsetup.sh
+++ bsetup.sh
@@ -6,4 +6,4 @@ sudo apt upgrade -y &&
 #zsh
 sudo apt install zsh -y &&
 sudo apt install git -y &&
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
